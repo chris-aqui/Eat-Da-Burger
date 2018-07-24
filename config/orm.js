09b02a6337b4b307563cad4964b701d9bed0b547
Christine <2739361+christine-aqui@users.noreply.github.com>
2018-07-23 21:12:55 -0400

    :tada: some setup

---
+++ config/orm.js
@@ -0,0 +1,9 @@
+// Import MySQL connection.
+const connection = require("../config/connection.js");
+
+
+
+//  ..
+
+// Export the orm object for the model (burger.js).
+module.exports = orm;
---
+++ models/burger.js
@@ -0,0 +1,10 @@
+// Import the ORM to create functions that will interact with the database.
+const orm = require("../config/orm.js");
+
+let burger = {
+  //  ...
+}
+
+
+// Export the database functions for the controller (controller.js).
+module.exports = burger;
